30fe676a6b52c3098bfd88469de4e23288c66e9d
zenitaeglos <aegloszenit@protonmail.com>
2020-01-18 18:30:38 +0100

    create static class to save and retrieve database data

---
+++ TuebixApp/DatabaseService/DatabaseService.swift
@@ -0,0 +1,15 @@
+//
+//  DatabaseService.swift
+//  TuebixApp
+//
+//  Created by Alejandro Martinez Montero on 18/01/2020.
+//  Copyright © 2020 Alejandro Martinez Montero. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class DatabaseService {
+    
+}
