c484dc30304a936dda24381d7df331af7e879a4d
zenitaeglos <aegloszenit@protonmail.com>
2020-03-21 17:28:15 +0100

    add a new tab with info and credits

---
+++ TuebixApp/Controllers/InfoViewController.swift
@@ -0,0 +1,32 @@
+//
+//  InfoViewController.swift
+//  TuebixApp
+//
+//  Created by Alejandro Martinez Montero on 21/03/2020.
+//  Copyright © 2020 Alejandro Martinez Montero. All rights reserved.
+//
+
+import UIKit
+import SafariServices
+
+class InfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    @IBAction func tuebixPressed(_ sender: UIButton) {
+        
+        if let url = URL(string: "https://www.tuebix.org") {
+            let config = SFSafariViewController.Configuration()
+            config.entersReaderIfAvailable = true
+            
+            let safariViewController = SFSafariViewController(url: url, configuration: config)
+            present(safariViewController, animated: true, completion: nil)
+        }
+    }
+}
